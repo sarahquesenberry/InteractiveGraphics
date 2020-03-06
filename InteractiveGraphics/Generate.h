56dbdc81c0d18b01042ae5023be1e0d21331bded
Sarah <sarahquesenberry2020@gmail.com>
2020-03-06 11:24:35 -0500

    lab7

--- InteractiveGraphics/Generate.h
+++ InteractiveGraphics/Generate.h
@@ -4,12 +4,15 @@
 #define GENERATE
 #include "OpenGLGraphicsObject.h"
 #include "GraphicsStructures.h"
+#include "OpenGLIndexedGraphicsObject.h"
 
 class Generate
 {
 public:
    static OpenGLGraphicsObject* FlatSurface(float width, float depth, RGBA color);
    static OpenGLGraphicsObject* Cuboid(float width, float depth, float height, RGBA color);
+
+   static OpenGLIndexedGraphicsObject* IndexedFlatSurface(float width, float depth, RGBA color);
 };
 
 #endif
---
+++ InteractiveGraphics/OpenGLIndexedGraphicsObject.h
@@ -0,0 +1,30 @@
+#pragma once
+#ifndef OPENGL_INDEXED_GRAPHICS_OBJECT
+#define OPENGL_INDEXED_GRAPHICS_OBJECT
+
+
+
+#include "OpenGLGraphicsObject.h"
+class OpenGLIndexedGraphicsObject :
+	public OpenGLGraphicsObject
+{
+protected:
+	GLuint _indexVbo;
+	vector<unsigned int> _indexes;
+
+public:
+	OpenGLIndexedGraphicsObject();
+	OpenGLIndexedGraphicsObject(AbstractGraphicsShader* shader);
+
+
+	void AddIndex(unsigned int index) { _indexes.push_back(index); }
+
+
+	void Setup();
+	void Render();
+
+
+
+};
+
+#endif
